09f5bd4ae842a428977d0ec05c50e4b38c900aff
任上旭 <renshangxu@gmail.com>
2024-09-06 00:55:07 +0800

    问题1第一版

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# 这是一个示例 Python 脚本。
+import numpy as np
+from scipy.optimize import fsolve
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+def next_theta(theta, k, d):
+    """θ角递推"""
+    def f(theta_next):
+        return theta**2 + theta_next**2 - 2 * theta * theta_next * np.cos(theta_next - theta) - d**2 / k**2
+    x = np.linspace(theta, theta + 3, 1000)
+    plt.plot(x, f(x))
+    plt.plot(x, np.zeros_like(x))
+    theta_next = fsolve(f, theta+1)
+    print(f"theta:{np.rad2deg(theta)},{theta}, k:{k}, d:{d}, theta_next:{np.rad2deg(theta_next)},{theta_next}")
+    plt.scatter(theta_next, 0)
+    plt.show()
+    return theta_next
 
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+next_theta(32.20, 0.0875, 1.65)
