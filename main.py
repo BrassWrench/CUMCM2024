44258b15b77503967d01b38357907d1f7542899c
任上旭 <renshangxu@gmail.com>
2024-09-08 10:43:10 +0800

    第五问调试

--- main.py
+++ main.py
@@ -37,27 +37,42 @@ if __name__ == '__main__':
     # problem2.save_collision_fig("problem2/savefig")
     # problem2.save_result()
 
-    problem3 = Problem3()
-    problem3.calc_collision_states(0.41, 0.55, 0.001, savefig=False)
-    problem3.save_pitch_fig("problem3/savefig")
+    # problem3 = Problem3()
+    # problem3.calc_collision_states(0.41, 0.55, 0.001, savefig=False)
+    # problem3.save_pitch_fig("problem3/savefig")
+    # print(problem3.pitch_critical)
+    # print(problem3.collision_radius_critical)
 
     #problem4_2 = Problem4_2()
     # problem4_2.save_curve("problem4/savefig")
     # problem4_2.save_r_xi("problem4/savefig")
 
-    # problem4_3 = Problem4_3()
+    problem4_3 = Problem4_3()
     # problem4_3.save_t_state(13, "problem4/savefig")
     # problem4_3.save_result()
 
-    # problem4_3 = Problem4_3()
-    # v = []
-    # t = np.arange(10, 20, 0.1)
-    # for t_now in tqdm(t, desc="第五问时间戳计算"):
-    #     x_n, y_n, v_n = problem4_3.get_t_state(t_now)
-    #     v.append(v_n)
-    #
-    # v = np.array(v)
-    # v = v.T
+    v = []
+    t = np.arange(10, 15, 0.01)
+    for t_now in tqdm(t, desc="第五问时间戳计算"):
+        x_n, y_n, v_n = problem4_3.get_t_state(t_now)
+        v.append(v_n)
+
+    v = np.array(v)
+    v = v.T
+    print(v.shape)
+    v_max = v[1]
+    plt.plot(t, v_max)
+
+    v_gradient = [(v_max[i + 1] - v_max[i]) / 0.01 for i in np.arange(len(t) - 1)]
+    plt.plot(t[:-1], v_gradient)
+    plt.savefig("v_max_and_gradient.pdf")
+    plt.cla()
+    plt.clf()
+    plt.close()
+
+    # print(np.argmax(v))
+    # problem4_3.save_t_state(13.2, ".")
+
     # for v_t in v:
     #     plt.plot(t, v_t, color="black")
     #
--- problem3/problem3.py
+++ problem3/problem3.py
@@ -1,15 +1,16 @@
-from tqdm import tqdm
 import numpy as np
-import matplotlib.pyplot as plt
 import time
-from scipy.optimize import bisect
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 from problem2.problem2 import Problem2
+
 
 class Problem3(Problem2):
 
     def __init__(self):
         super().__init__()
         self.pitch_critical = None
+        self.collision_radius_critical = None
         self.theta0_critical = None
         self.rectangles_critical = None
         self.x_critical = None
@@ -27,9 +28,18 @@ class Problem3(Problem2):
         return collision_radius
 
     def calc_collision_states(self, pmin, pmax, interval, savefig=False):
-
+        pass_critical = False
         for p in tqdm(np.arange(pmin, pmax, interval), desc="对每个螺距求解碰撞半径"):
             collision_radius = self.get_collision_radius(p, savefig)
+            if collision_radius < 4.5 and not pass_critical:
+                pass_critical = True
+                self.pitch_critical = p
+                self.collision_radius_critical = collision_radius
+                self.theta0_critical = self.theta0_collision
+                self.rectangles_critical = self.rectangles_collision
+                self.x_critical = self.x_collision
+                self.y_critical = self.y_collision
+                self.v_critical = self.v_collision
             self.collision_radius[str(p)] = collision_radius
 
     def save_pitch_fig(self, direct):
@@ -38,11 +48,11 @@ class Problem3(Problem2):
             pitch.append(float(key))
             collision_radius.append(value)
         plt.plot(pitch, collision_radius, color="blue", zorder=1)
-        #plt.scatter(pitch, collision_radius, color="green", zorder=2)
+        plt.scatter(self.pitch_critical, self.collision_radius_critical, color="red", zorder=2)
         plt.plot(pitch, np.ones_like(pitch) * 4.5, linestyle='--', color="orange", zorder=3)
         plt.grid()
         plt.xlabel("螺距p(m)")
-        plt.ylabel("碰撞半径", rotation="horizontal")
+        plt.ylabel("碰撞半径", rotation="horizontal", labelpad=10)
         plt.savefig(f"{direct}/pitch_and_collision_radius.pdf")
         plt.cla()
         plt.clf()
